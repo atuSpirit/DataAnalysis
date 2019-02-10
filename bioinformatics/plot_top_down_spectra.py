1189bb0c4518a6aefb852c7ef91e70c55d7dd0d1
U-BSI\hlin <hlin@dijkstra.bioinformaticssolutions.com>
2019-02-10 15:34:43 -0500

    plot top down spectra with true peaks and noise peaks

--- bioinformatics/plot_top_down_spectra.py
+++ bioinformatics/plot_top_down_spectra.py
@@ -35,34 +35,31 @@ def transform_peaks_to_bar_list(peaks_dataframe, height_index):
 
 
 dir = "D:/Hao/data/for_analysis/result"
-real_peaks_file = dir + "/real_peaks.txt"
-noise_peaks_file = dir + "/noise_peaks.txt"
+real_peaks_file = dir + "/real_peaks.valuablePeak.twoAA.all.txt"
+noise_peaks_file = dir + "/noise_peaks.valuablePeak.twoAA.all.txt"
 
 real_peaks = load_peaks(real_peaks_file)
 noise_peaks = load_peaks(noise_peaks_file)
 
 #Draw intensities of real peaks vs noise peaks
 height_index = 4
-fig_title = "Intensity of Real peaks vs noise peaks of filtered peaks using one AA tag"
-fig_file = dir + "/fig/intensity.real.vs.noise_peaks of filtered peaks using one AA tag.svg"
+fig_title = "Intensity of Real peaks vs noise peaks of filtered peaks using one or two AA tag"
+fig_file = dir + "/fig/intensity.real.vs.noise_peaks.filteredByTwoAAtag.svg"
 
 
 real_peak_bar_list = transform_peaks_to_bar_list(real_peaks, height_index)
 noise_peak_bar_list = transform_peaks_to_bar_list(noise_peaks, height_index)
 draw_peaks(real_peak_bar_list, noise_peak_bar_list, fig_title, fig_file)
 
-"""
+
 #Draw supporting number of peaks
 height_index = 1
-fig_title = "Supporting number of Real peaks vs noise peaks of one scan"
-fig_file = dir + "/fig/supporting_num.1.real.vs.noise_peaks.svg"
+fig_title = "Supporting number of Real peaks vs noise peaks using one or two AA tag"
+fig_file = dir + "/fig/supporting_num.1.real.vs.noise_peaks..filteredByTwoAAtag.svg"
 
 real_peak_bar_list = transform_peaks_to_bar_list(real_peaks, height_index)
 noise_peak_bar_list = transform_peaks_to_bar_list(noise_peaks, height_index)
 draw_peaks(real_peak_bar_list, noise_peak_bar_list, fig_title, fig_file)
-
-"""
-
 
 
 
